28c2ed4ecb8b8f502fc8661e2cf6d75136478a64
splitofyou <waysssghoul@gmail.com>
2023-10-15 20:21:35 +0300

    add: image(hero picture)

---
+++ Main_Hero.py
@@ -0,0 +1,5 @@
+import pygame
+
+class MainHero():
+    def __init__(self, image):
+        self.image = pygame.image.load(hero.png)
--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import pygame
 import sys
 
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((1000, 800))
+    pygame.display.set_caption("WAY2DIE")
+
+    flag = True
+    while flag:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        pygame.display.flip()
+
+
+run_game()
+
+
