8ede36187e53897823b69833934844ed7b2306b3
splitofyou <waysssghoul@gmail.com>
2023-10-19 21:53:01 +0300

    add: hero moving

--- Main_Hero.py
+++
@@ -1,5 +0,0 @@
-import pygame
-
-class MainHero():
-    def __init__(self, image):
-        self.image = pygame.image.load(hero.png)
--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import pygame
 import sys
-
-def run_game():
+from main_character import MainCharacter
+def start_game():
     pygame.init()
     screen = pygame.display.set_mode((1000, 800))
     pygame.display.set_caption("WAY2DIE")
+    maincharacter = MainCharacter(screen)
 
     flag = True
     while flag:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    maincharacter.rect.centerx += 1
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    maincharacter.move_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_d:
+                    maincharacter.move_right = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    maincharacter.rect.centerx -= 1
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    maincharacter.move_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_a:
+                    maincharacter.move_left = False
 
+        maincharacter.output()
         pygame.display.flip()
 
-
-run_game()
-
-
+start_game()
